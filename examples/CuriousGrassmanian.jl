1d23b0aaa1b8a0fc90b52450b2edc7079f57761e
JavierFernandezPiriz <javferp@gmail.com>
2026-04-21 14:33:44 +0200

    feat: examples
    Add BottVanishingE6.jl, example that checks wether maximal parabolics of E6 satisfy or not Bott Vanishing
    
    Add CuriousGrassmanian.jl, example that checks if the code can provide if "curious" grassmanians have non-global Hochschild cohomology

---
+++ examples/CuriousGrassmanian.jl
@@ -0,0 +1,29 @@
+# ═══════════════════════════════════════════════════════════════════════════════
+#  CuriousGrassmanian.jl — Hochschild Cohomology for OGr(n-1,2n+1), n ≥ 4
+# ═══════════════════════════════════════════════════════════════════════════════
+
+using PartialFlagVarieties
+
+function main()
+    for n in 4:8
+        X = OGr(n - 1, 2 * n + 1)
+        T = filtered_tangent_bundle(X)
+        flag = true
+        for q in 1:rank_bundle(T)
+            if has_higher_cohomology(exterior_power(T, q))
+                msg = "Hochschild cohomology is not global for OGr($a, $b) " *
+                      "in degree q = $q"
+                println(msg)
+                flag = false
+                break
+            end
+        end
+        if flag
+            msg = "Not enough information to conclude non-global Hochschild " *
+                  "cohomology for OGr($a, $b)"
+            println(msg)
+        end
+    end
+end
+
+main()
